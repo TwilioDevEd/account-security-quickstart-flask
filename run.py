42e319b71b5222a0476372c574de0983ac188349
mikespub <mikespub.net@hotmail.com>
2020-10-17 14:19:43 +0000

    try out Authy 2FA behind proxy

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from twofa import app, init_db
 
 
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0', port=8080)
